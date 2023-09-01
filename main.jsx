c5f7bd29730f93fde04f147eb7b9b782d246dafc
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-01 00:12:07 -0500

    add main files

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
-import {createRoot} from 'react-dom/client'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+import App from './src/App.jsx'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
 root.render(
-    <App/>
+  <App />
 )
