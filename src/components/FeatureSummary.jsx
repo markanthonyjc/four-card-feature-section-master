0b079f36e3af010baefa47464d582d2e45362a76
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-01 21:23:25 -0500

    Update content and add styles

---
+++ src/components/FeatureSummary.jsx
@@ -0,0 +1,13 @@
+import style from './FeatureSummary.module.scss'
+
+const FeatureSummary = () => {
+  return <section className={style['feature-summary']}>
+    <p>Reliable, efficient delivery</p>
+    <p>Powered by Technology</p>
+    <p>
+      Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful
+    </p>
+  </section>
+}
+
+export default FeatureSummary
