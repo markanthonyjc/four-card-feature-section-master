b86961a5b1340d31f1c3b59763d7dbc20739ce79
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-02 20:42:17 -0500

    Add content and styles for card component

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,5 +1,15 @@
-const Card = () => {
-  return <></>
+import style from './Card.module.scss'
+
+const Card = ({title, description, icon, modifier}) => {
+  return <div className={`${style.card} ${style[`card--${modifier}`]}`}>
+    <div className={style.summary}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+    <div className={style.image}>
+      {icon && <img src={icon} alt="A magnifying glass with an eye inside"/>}
+    </div>
+  </div>
 }
 
 export default Card
