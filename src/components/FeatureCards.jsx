8cf2c2892d18c2a7d35a9fab83ddb6125a159c58
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-02 20:44:18 -0500

    Add content and styles for feature cards component

--- src/components/FeatureCards.jsx
+++ src/components/FeatureCards.jsx
@@ -1,6 +1,36 @@
+import Card from '../UI/Card.jsx'
+import { IconCalculator, IconKarma, IconSupervisor, IconTeamBuilder } from '../assets/images'
+import style from './FeatureCards.module.scss'
+
 const FeatureCards = () => {
-  return <>
-  </>
+  return <section className={style['feature-cards']}>
+    <div>
+      <Card title={'Supervisor'}
+            description={'Monitors activity to identity project roadblocks'}
+            icon={IconSupervisor}
+            modifier={'supervisor'}
+      />
+    </div>
+    <div>
+      <Card title={'Team Builder'}
+            description={'Scans out talent network to create the optimal team for your project'}
+            icon={IconTeamBuilder}
+            modifier={'team-builder'}
+      />
+      <Card title={'Karma'}
+            description={'Regularly evaluates out talent to ensure quality'}
+            icon={IconKarma}
+            modifier={'karma'}
+      />
+    </div>
+    <div>
+      <Card title={'Calculator'}
+            description={'Uses data from past projects to provider better delivery estimates'}
+            icon={IconCalculator}
+            modifier={'calculator'}
+      />
+    </div>
+  </section>
 }
 
 export default FeatureCards
