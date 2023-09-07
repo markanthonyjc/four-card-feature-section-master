fed23f729221b1495c49aa8280ad435c50d631cf
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-06 23:15:18 -0500

    Add modifier class dictionary and update styles and reference

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,13 +1,13 @@
 import style from './Card.module.scss'
 
 const Card = ({title, description, icon, modifier}) => {
-  return <div className={`${style.card} ${style[`card--${modifier}`]}`}>
+  return <div className={`${style.card} ${style[modifier]}`}>
     <div className={style.summary}>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
     <div className={style.image}>
-      {icon && <img src={icon} alt="A magnifying glass with an eye inside"/>}
+      {icon && <img src={icon} alt=""/>}
     </div>
   </div>
 }
--- src/components/FeatureCards.jsx
+++ src/components/FeatureCards.jsx
@@ -2,32 +2,39 @@ import Card from '../UI/Card.jsx'
 import { IconCalculator, IconKarma, IconSupervisor, IconTeamBuilder } from '../assets/images'
 import style from './FeatureCards.module.scss'
 
+const cardModifiers = {
+  SUPERVISOR: 'card--supervisor',
+  TEAM_BUILDER: 'card--team-builder',
+  KARMA: 'card--karma',
+  CALCULATOR: 'card--calculator'
+}
+
 const FeatureCards = () => {
   return <section className={style['feature-cards']}>
     <div>
       <Card title={'Supervisor'}
             description={'Monitors activity to identity project roadblocks'}
             icon={IconSupervisor}
-            modifier={'supervisor'}
+            modifier={cardModifiers.SUPERVISOR}
       />
     </div>
     <div>
       <Card title={'Team Builder'}
             description={'Scans out talent network to create the optimal team for your project'}
             icon={IconTeamBuilder}
-            modifier={'team-builder'}
+            modifier={cardModifiers.TEAM_BUILDER}
       />
       <Card title={'Karma'}
             description={'Regularly evaluates out talent to ensure quality'}
             icon={IconKarma}
-            modifier={'karma'}
+            modifier={cardModifiers.KARMA}
       />
     </div>
     <div>
       <Card title={'Calculator'}
             description={'Uses data from past projects to provider better delivery estimates'}
             icon={IconCalculator}
-            modifier={'calculator'}
+            modifier={cardModifiers.CALCULATOR}
       />
     </div>
   </section>
