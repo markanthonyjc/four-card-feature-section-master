705682f1aae3b3187eed7924a6f5ca9644b635be
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-10 13:28:39 -0500

    Install gh-pages package

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/four-card-feature-section-master/',
   plugins: [react()]
 })
